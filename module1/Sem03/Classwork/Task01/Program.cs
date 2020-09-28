7b633613189ae45b57bec3ec72f66914da69f00c
Владислав Пендищук <vmpendischuk@edu.hse.ru>
2020-09-28 19:29:40 +0300

    Remade folder heirarchy

---
+++ module1/Sem03/Classwork/Task01/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Task01
+{
+    class Program
+    {
+        // Метод, переводящий оценку в баллах десятибалльной шкалы в аттестационную шкалу.
+        public static void Interpret(int mark)
+        {
+            switch (mark)
+            {
+                case 1:
+                case 2:
+                case 3:
+                    Console.WriteLine("Неудовлетворительно");
+                    break;
+                case 4:
+                case 5:
+                    Console.WriteLine("Удовлетворительно");
+                    break;
+                case 6:
+                case 7:
+                    Console.WriteLine("Хорошо");
+                    break;
+                case 8:
+                case 9:
+                case 10:
+                    Console.WriteLine("Отлично");
+                    break;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int num;
+            int.TryParse(Console.ReadLine(), out num);
+            Interpret(num);
+        }
+    }
+}
---
+++ module1/Sem03/Classwork/Task02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task02
+{
+    class Program
+    {
+        // Метод, возвращающий истинностное значение выражения !(p & q) & !(p | !q) для данных значений атомов p, q.
+        public static bool Function(bool p, bool q)
+        {
+            return !(p & q) & !(p | !q);
+        }
+
+        static void Main(string[] args)
+        {
+            bool q, res;
+            bool p = true;
+            Console.WriteLine(" p \t q \t F");
+            do
+            {
+                q = true;
+                do
+                {
+                    res = Function(p, q);
+                    Console.WriteLine($"{Convert.ToInt16(p)}\t{Convert.ToInt16(q)}\t{Convert.ToInt16(res)}");
+                    q = !q;
+                } while (!q);
+                p = !p;
+            } while (!p);
+        }
+    }
+}
