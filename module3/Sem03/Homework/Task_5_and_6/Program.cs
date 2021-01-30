850b0e6c40712dc5c5aacdce8cdf7ffbc3f51da0
NicNieRobie <70844046+NicNieRobie@users.noreply.github.com>
2021-01-30 16:23:27 +0300

    Sem03 added.

---
+++ module3/Sem03/Homework/Task_5_and_6/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace ConsoleApp14
+{
+    public delegate void VoidOperation();
+    public delegate void ItemEvents(int[,] arr);
+
+    static class ArrayOperations
+    {
+        public static event VoidOperation OnLineEnd;
+        public static event ItemEvents NewItemFilled;
+
+        public static void PrintArray(int[,] arr)
+        {
+
+            int cntr = 0;
+            for (int i = 0; i <= arr.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= arr.GetUpperBound(1); j++)
+                {
+                    Console.Write(arr[i, j] + " \t");
+                    cntr++;
+                    if (cntr % 5 == 0) OnLineEnd?.Invoke();
+                }
+            }
+        }
+
+        public static void FillArray(int[,] arr, int lowerBound = 0, int upperBound = 100)
+        {
+            Random rand = new Random();
+            for (int i = 0; i <= arr.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= arr.GetUpperBound(1); j++)
+                {
+                    arr[i, j] = rand.Next(lowerBound, upperBound);
+                    NewItemFilled?.Invoke(arr);
+                }
+            }
+        }
+
+        private static int CalculateArraySum(int[,] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i <= arr.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= arr.GetUpperBound(1); j++)
+                {
+                    sum += arr[i, j];
+                }
+            }
+
+            return sum;
+        }
+
+        public static void ArraySum(int[,] arr)
+        {
+            int sum = CalculateArraySum(arr);
+            Console.WriteLine("Sum: " + sum);
+        }
+
+        public static void ArrayAverage(int[,] arr)
+        {
+            int sum = CalculateArraySum(arr);
+            Console.WriteLine("Average: " + (double)sum / arr.Length);
+        }
+
+        public static void ArrayMaximum(int[,] arr)
+        {
+            int max = int.MinValue;
+            for (int i = 0; i <= arr.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= arr.GetUpperBound(1); j++)
+                {
+                    if (arr[i, j] > max) max = arr[i, j];
+                }
+            }
+
+            Console.WriteLine("Maximum value: " + max);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = new int[6, 6];
+            ArrayOperations.NewItemFilled += ArrayOperations.ArraySum;
+            ArrayOperations.NewItemFilled += ArrayOperations.ArrayAverage;
+            ArrayOperations.NewItemFilled += ArrayOperations.ArrayMaximum;
+
+            ArrayOperations.FillArray(arr);
+
+            ArrayOperations.OnLineEnd += () => { Console.WriteLine(); };
+            Console.WriteLine();
+            ArrayOperations.PrintArray(arr);
+        }
+    }
+}
---
+++ module3/Sem03/Homework/VotingTask/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace VotingTask
+{
+    class VetoVoter
+    {
+        public string Name { get; private set; }
+
+        public void VetoVote(object sender, VetoEventArgs e)
+        {
+            Random rand = new Random();
+            if (e.VetoBy != null) return;
+            if (rand.Next(0, 5) == 1) e.VetoBy = this;
+        }
+
+        public VetoVoter(string name)
+        {
+            Name = name;
+        }
+    }
+
+    class VetoComission
+    {
+        public event EventHandler<VetoEventArgs> OnVote;
+
+        public VetoEventArgs Vote(string Proposal)
+        {
+            VetoEventArgs vetoVoteArgs = new VetoEventArgs(Proposal);
+            OnVote?.Invoke(this, vetoVoteArgs);
+            return vetoVoteArgs;
+        }
+    }
+
+    class VetoEventArgs : EventArgs
+    {
+        public string Proposal { get; set; }
+
+        public VetoVoter VetoBy { get; set; }
+
+        public VetoEventArgs(string proposal)
+        {
+            Proposal = proposal;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VetoComission vetoCom = new VetoComission();
+            VetoVoter[] voters = new VetoVoter[5];
+            string[] voterNames = { "John", "Albert", "Giovanni", "Elisa", "Sean"};
+
+            for (int i = 0; i < voters.Length; i++)
+            {
+                voters[i] = new VetoVoter(voterNames[i]);
+                vetoCom.OnVote += voters[i].VetoVote;
+            }
+
+            VetoEventArgs voteResults = vetoCom.Vote("Lifting lockdown");
+
+            Console.WriteLine($"Proposal: {voteResults.Proposal}. Vote result: {(voteResults.VetoBy == null ? "Approved" : "Vetoed")}.");
+            if (voteResults.VetoBy != null) Console.WriteLine($"Vetoed by: {voteResults.VetoBy.Name}.");
+        }
+    }
+}
